101d0dbb64eba0659b27ed6211cd7209e36004fe
nicholas <nicholas@zainaweb.com>
2022-04-01 09:25:33 +1100

    item plcaed correctly

---
+++ pages/messenger.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function messenger() {
+  return (
+     <div className='flex-1'>
+    <div className="bg-blue-600 rounded-full w-16 h-16 m-4 justify-end">
+      button
+    </div>
+
+    </div>
+  )
+}
+
+export default messenger
